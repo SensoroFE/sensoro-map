aa781751003b975a37c2563a68da90883c241372
bhcbhc <ninghai@sensoro.com>
2022-05-13 21:34:54 +0800

    feat: 支持搜索下拉功能

--- src/components/position-selector/components/search-address/index.tsx
+++ src/components/position-selector/components/search-address/index.tsx
@@ -4,6 +4,7 @@ import { Input, AutoComplete as AntAutoComplete } from "antd";
 import { AutoCompleteProps } from "@pansy/react-amap/es/auto-complete/types";
 import { LngLatObj } from "../../../../map/types";
 import { useMap, AutoComplete } from "@pansy/react-amap";
+import { useClickAway } from "ahooks";
 
 export interface SearchAddressProps extends AutoCompleteProps {
   prefixCls?: string;
@@ -14,14 +15,25 @@ export interface SearchAddressProps extends AutoCompleteProps {
 
 const SearchAddress: FC<SearchAddressProps> = (props) => {
   const autoComplete = useRef<AMap.AutoComplete>();
+  const clickRef = useRef();
   const { map } = useMap();
   const { prefixCls, onChange, size, city } = props;
+  const [searchVal, setSearchVal] = useState<string | undefined>(undefined);
   const [options, setOptions] = useState([]);
+  const [visible, setVisible] = useState<boolean>(false);
 
   useEffect(() => {
     autoComplete?.current?.setCity(city || "全国");
     autoComplete?.current?.setCityLimit(!!city);
   }, [city]);
+
+  useClickAway(() => {
+    setVisible(false);
+  }, clickRef);
+
+  useEffect(() => {
+    if(!searchVal) setVisible(false);
+  }, [searchVal]);
 
   const handleSelect = (value: string, options: any) => {
     const location = options.location;
@@ -35,34 +47,33 @@ const SearchAddress: FC<SearchAddressProps> = (props) => {
   };
 
   const handleSearch = (value: string) => {
-    if (!autoComplete.current) return;
+    if (!autoComplete.current || !value.trim()) return;
     autoComplete.current.search(value, (status, results) => {
       if (status === "complete") {
-        const tips = results.tips.filter((item) => item.id).map(renderItem);
-
+        const tips = results.tips.filter((item) => item.id);
+        console.log("tips", tips);
         setOptions(tips);
+        setVisible(true);
+      } else {
+        setOptions([]);
+        setVisible(true);
       }
     });
   };
 
   const renderItem = (item: AMap.AutoComplete.Tip) => {
-    return {
-      ...item,
-      value: item.name,
-      key: uniqueId("address_"),
-      label: (
-        <div>
-          {item.name}
-          <span style={{ paddingLeft: 4, color: "rgba(0, 0, 0, 0.45)" }}>
-            {item.district}
-          </span>
-        </div>
-      ),
-    };
+    return (
+      <div key={item.id} className={`${prefixCls}-dropdown-item`}>
+        <p>{item.name}</p>
+        <p>{item.district}</p>
+      </div>
+    );
   };
 
+  const empty = <p className={`${prefixCls}-empty`}>无搜索结果</p>;
+
   return (
-    <span className={prefixCls}>
+    <div className={prefixCls} ref={clickRef}>
       <AutoComplete
         events={
           {
@@ -74,11 +85,23 @@ const SearchAddress: FC<SearchAddressProps> = (props) => {
       />
       <Input.Search
         onSearch={handleSearch}
+        onChange={(e) => {
+          const val = e.target.value;
+          setSearchVal(val);
+          handleSearch(val);
+        }}
+        value={searchVal}
         style={{ width: size === "small" ? 200 : 240 }}
         size={size}
+        allowClear
         placeholder="请输入地址信息"
       />
-    </span>
+      {visible && (
+        <div className={`${prefixCls}-dropdown`}>
+          {options.length ? <>{options.map((i) => renderItem(i))}</> : empty}
+        </div>
+      )}
+    </div>
   );
 };
 
